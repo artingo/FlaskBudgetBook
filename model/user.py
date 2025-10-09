3e816d950aafe56ad7107d27bf157f04af9ea085
artingo <alfred.walther@artingo.net>
2025-10-09 21:51:58 +0200

    Migrated user pages to Bootstrap 5

--- model/user.py
+++ model/user.py
@@ -3,3 +3,7 @@ class User:
     def __init__(self, firstname: str, lastname: str):
         self.firstname = firstname
         self.lastname = lastname
+
+    @property
+    def fullname(self) -> str:
+        return f"{self.firstname} {self.lastname}"
